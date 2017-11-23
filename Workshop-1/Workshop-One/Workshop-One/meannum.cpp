2d05ae8628beaf8abbdbff12b009343dc9161858
Ryan <ryanimpey@live.co.uk>
2017-11-23 17:58:29 +0000

    Workshop One
    Awful calculator which allows plus, minus, times, divide, sin, cos, and tan, also allows mean value calculation.

---
+++ Workshop-1/Workshop-One/Workshop-One/meannum.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include <iostream>
+#include "meanum.h"
+
+double meanNum(double inputNum, double currentValue) {
+	return currentValue + inputNum;
+}
---
+++ Workshop-1/Workshop-One/Workshop-One/meanum.h
@@ -0,0 +1,3 @@
+#pragma once
+
+double meanNum(double inputNum, double currentValue);
