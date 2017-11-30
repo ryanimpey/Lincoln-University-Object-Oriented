adf09fb4d0688292e458dc75a57e81b492315262
x <x>
2017-11-30 15:17:47 +0000

    Virtual Functions part One

---
+++ Workshop-9/Workshop9/Workshop9/Source.cpp
@@ -0,0 +1,18 @@
+#include "ab.h"
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+	//don’t change any of the following
+	B b;
+	A *aP = &b;
+	cout << aP->report() << endl; // want this to produce “I am child class”
+
+	int breakP = 0;
+	cin >> breakP;
+
+	return 0;
+
+}
