1a076c0b9b7a997afa9287937f24252bad5e0f48
Ryan <ryanimpey@live.co.uk>
2017-12-14 15:26:59 +0000

    Task 2: Stack unwinding

--- Workshop-11/Workshop11/Workshop11/Workshop11/Source.cpp
+++ Workshop-11/Workshop11/Workshop11/Workshop11/Source.cpp
@@ -3,13 +3,45 @@
 
 using namespace std;
 
-void main() {	try
-	{	
-		//Required to be in string() as compilier will try throwing a char[] array otherwise which will be unhandled.
-		throw string("error");
+void f1(int k) {
+	string err = "F1 ERROR";
+	if (!k) {
+		throw err;
+		cout << "f1: error!" << endl;
 	}
-	catch (std::string e)
-	{
-		std::cout << "Exception caught: " << e << std::endl;
+	else {
+		cout << "f1 success" << endl;
+	}
+}
+
+void f2(int j) {
+	try {
+		f1(j);
+	}
+	catch (string e) {
+		cout << "exception caught in f2" << endl;
+		throw e;
+	}
+	cout << "f2 success" << endl;
+}void f3(int i) {
+	f2(i);
+	cout << "f3 success" << endl;
+}
+
+void main() {	try {
+		f3(0); //compare this to situation where argument is 0
+	}
+	catch (string e) {
+		cout << "Caught exception: " << e << endl;
 	}	int endProg = 0;	cout << "Press any key to continue..." << endl;	cin >> noskipws >> endProg;
+	//// Part One	//try
+	//{	
+	//	//Required to be in string() as compilier will try throwing a char[] array otherwise which will be unhandled.
+	//	throw string("error");
+	//}
+	//catch (std::string e)
+	//{
+	//	std::cout << "Exception caught: " << e << std::endl;
+	//}
+
 }
