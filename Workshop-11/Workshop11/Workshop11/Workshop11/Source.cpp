7ad06fd8dde10c1b438099274928ae412df6938e
Ryan <ryanimpey@live.co.uk>
2017-12-14 15:18:57 +0000

    Workshop-11 and task 1

---
+++ Workshop-11/Workshop11/Workshop11/Workshop11/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void main() {	try
+	{	
+		//Required to be in string() as compilier will try throwing a char[] array otherwise which will be unhandled.
+		throw string("error");
+	}
+	catch (std::string e)
+	{
+		std::cout << "Exception caught: " << e << std::endl;
+	}	int endProg = 0;	cout << "Press any key to continue..." << endl;	cin >> noskipws >> endProg;
+}
